b873728e1ba41c97b9a2cd03e65ab5e6d53a4f3e
Vadim Khalikov <vkhalikov.work@gmail.com>
2019-12-13 02:59:23 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+      e.preventDefault();
+      const offset = document.querySelector('.navbar').offsetHeight;
+      const element = document.querySelector(this.getAttribute('href'));
+			const bodyRect = document.body.getBoundingClientRect().top;
+			const elementRect = element.getBoundingClientRect().top;
+			const elementPosition = elementRect - bodyRect;
+			const offsetPosition = elementPosition - offset;
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth',
-            block: "start"
-        });
+			window.scrollTo({
+				top: offsetPosition,
+				behavior: 'smooth'
+			}); 
     });
 });
-
